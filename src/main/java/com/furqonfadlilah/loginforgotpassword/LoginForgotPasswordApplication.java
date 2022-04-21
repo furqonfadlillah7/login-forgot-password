51807ef15b92085546a08e5233a6d6e6782a1149
furqonfadlillah7 <furqonfadlillah7@gmail.com>
2022-04-22 05:06:47 +0700

    initial commit

---
+++ src/main/java/com/furqonfadlilah/loginforgotpassword/LoginForgotPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.furqonfadlilah.loginforgotpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginForgotPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginForgotPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furqonfadlilah/loginforgotpassword/LoginForgotPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furqonfadlilah.loginforgotpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginForgotPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
